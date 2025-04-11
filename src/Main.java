c1f0f2141610d768b526b144e2942f96832d5b0c
Eugene Demenko <eugenekiberone@gmail.com>
2025-04-11 17:53:03 +0300

    added variable

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int a = 5;
         int b = 7;
-        System.out.println(a+b);
+        int c = 7;
+        System.out.println(a+b+c);
     }
 }
