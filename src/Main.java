fb0ddb32b06444918c43265776bd762ead46e0d0
Eugene Demenko <eugenekiberone@gmail.com>
2025-04-11 17:50:08 +0300

    changed output. added variable

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Cyberjump!");
+        int a = 5;
+        int b = 7;
+        System.out.println(a+b);
     }
 }
