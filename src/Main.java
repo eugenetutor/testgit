28368d374cd1cb01d4d7a6be1049336b65709733
Eugene Demenko <eugenekiberone@gmail.com>
2025-04-18 17:01:22 +0300

    added new rows

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
         int b = 7;
         int c = 7;
         System.out.println(a+b+c);
+        String tokenAPI = "402880824ff933a4014ff9345d7c0002";
+        System.out.println(tokenAPI);
     }
 }
