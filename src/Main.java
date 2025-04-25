918b8ef7033faece71f17d75708ce9371c8b66d5
Ducnskd <a18999568@gmail.com>
2025-04-25 20:45:22 +0300

    Added Apicheckerlink from artur

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,6 @@ public class Main {
 
         System.out.println("hi");
         String DevName1 = "Arthur";
+        String Apichecker = "https://raw.githubusercontent.com/planetyadv/Df_da/refs/heads/main/ddd.md";
     }
 }
